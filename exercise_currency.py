d140809a0792e31859b0aa461fd96faf8ebb1f4d
Janaan-Yapur <jyapur@mail.austral.edu.ar>
2026-03-15 18:32:14 -0300

    Update exercise_currency.py

--- exercise_currency.py
+++ exercise_currency.py
@@ -11,7 +11,8 @@ def currency():
     tasa_dolar = 1500  # 1 dólar = 1500 pesos
     tasa_euro = 1600   # 1 euro = 1600 pesos
     tasa_real = 250    # 1 real = 250 pesos
-
+    
+    """
 Ejercicio 13 - Conversión de Moneda
 
     Dado un monto en pesos argentinos y tasas de cambio, imprimir:
