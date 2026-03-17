da9276d117832b0148660bd86d1e10a00ab6a368
Janaan-Yapur <jyapur@mail.austral.edu.ar>
2026-03-17 09:19:32 -0300

    Update exercise_circle.py

--- exercise_circle.py
+++ exercise_circle.py
@@ -10,15 +10,6 @@ def circle():
     2. La circunferencia (2 × π × radio)
     """
     radio = 5
-    
-    """
-    Ejercicio 6 - Geometría de Círculo
-
-    Dado el radio de un círculo, calcular e imprimir:
-    1. El área (π × radio²)
-    2. La circunferencia (2 × π × radio)
-    """
-    radio = 5
     area = math.pi * radio ** 2
     print(area)
     circunferencia = 2 * math.pi * radio
