f3cfb2d8651461ab26a07af87994644f2231f601
Janaan-Yapur <jyapur@mail.austral.edu.ar>
2026-03-17 09:24:43 -0300

    Update exercise_swap.py

--- exercise_swap.py
+++ exercise_swap.py
@@ -10,18 +10,6 @@ def swap():
     """
     x = 10
     y = 20
-
-    """
-     Ejercicio 9 - Intercambio de Variables
-
-    Dados dos valores x e y, intercambiar sus valores e imprimir:
-    1. El valor original de x
-    2. El valor original de y
-    3. El valor de x después del intercambio
-    4. El valor de y después del intercambio
-    """
-    x = 10
-    y = 20
     valor_og_x = (x)
     print(valor_og_x)
     valor_og_y = (y)
@@ -31,4 +19,3 @@ def swap():
     valor_y = (y // 2)
     print(valor_y)
 
-swap()
