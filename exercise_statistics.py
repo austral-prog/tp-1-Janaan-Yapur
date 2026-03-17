42529f33439659b875eccdf3aa92c5971e2d97b9
Janaan-Yapur <jyapur@mail.austral.edu.ar>
2026-03-17 09:24:15 -0300

    Update exercise_statistics.py

--- exercise_statistics.py
+++ exercise_statistics.py
@@ -12,20 +12,7 @@ def statistics():
     num2 = 8
     num3 = 23
     num4 = 12
-    
-    """
-      Ejercicio 5 - Estadísticas Simples
 
-    Dados cuatro números, calcular e imprimir:
-    1. El promedio
-    2. El máximo
-    3. El mínimo
-    4. El rango (diferencia entre máximo y mínimo)
-    """
-    num1 = 15
-    num2 = 8
-    num3 = 23
-    num4 = 12
     promedio = (num1 + num2 + num3 + num4) / 4
     print(promedio)
     maximo = max(num1, num2, num3, num4)
