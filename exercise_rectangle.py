f361bdb65a0af738eac6081555489ff3e6b59329
Janaan-Yapur <jyapur@mail.austral.edu.ar>
2026-03-17 09:23:53 -0300

    Update exercise_rectangle.py

--- exercise_rectangle.py
+++ exercise_rectangle.py
@@ -8,23 +8,7 @@ def rectangle():
     """
     base = 10
     altura = 5
-    
-    """
-     Ejercicio 8 - Cálculo de Precio Final
-
-    Dado un precio base, calcular e imprimir:
-    1. El monto del impuesto (21%)
-    2. El subtotal (precio base + impuesto)
-    3. El monto de la propina (10% del subtotal)
-    4. El precio final (subtotal + propina)
-    """
-    precio_base = 100
-    impuesto = (precio_base * 0.21)
-    print(impuesto)
-    subtotal = (impuesto + precio_base)
-    print(subtotal)
-    propina = (subtotal * 0.10)
-    print(propina)
-    final = (subtotal + propina)
-    print(final)
-
+    area = (base * altura)
+    print(area)
+    perimetro = ((2 * base) + (2 * altura))
+    print(perimetro)
